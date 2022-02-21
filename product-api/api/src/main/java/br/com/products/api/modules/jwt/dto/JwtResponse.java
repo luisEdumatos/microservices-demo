bde262134f2d2e24453d5c4fe394916b1d792441
luisedu.unifal <luisedu.unifal@gmail.com>
2022-02-21 09:49:05 -0300

    Interceptor authentication

---
+++ product-api/api/src/main/java/br/com/products/api/modules/jwt/dto/JwtResponse.java
@@ -0,0 +1,28 @@
+package br.com.products.api.modules.jwt.dto;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
+
+    private Integer id;
+    private String name;
+    private String email;
+
+    public  static JwtResponse getUser(Claims jwtClaims) {
+        try {
+            return new ObjectMapper().convertValue(jwtClaims.get("authUser"), JwtResponse.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+}
